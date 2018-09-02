1d86412cc6d9f9b36b5fd2f6977339fa23fe3c5c
mana-ysh <manabe.hitoshi.me0@is.naist.jp>
2018-09-02 19:53:53 +0900

    Various change

--- src/taylor.py
+++ src/taylor.py
@@ -8,8 +8,9 @@ class TaylorLowRegressor():
     def __init__(self):
         pass
 
-    def fit(self, word_seq, width):
-        word2posit = defaultdict(lambda x: [])
+    def fit(self, word_seq, width=5620):
+        print("processing text...")
+        word2posit = defaultdict(list)
         for i, word in enumerate(word_seq):
             word2posit[word].append(i)
 
@@ -19,19 +20,19 @@ class TaylorLowRegressor():
         freq_list = []
         posit_list = []
         # FIXME: slow
-        for i, w, posits in enumerate(word2posit.items()):
+        for w, posits in word2posit.items():
             freqs = []
             for j in range(len(word_seq) // width + 1):
                 start_idx = j * width
                 end_idx = (j+1) * width
-                freqs.append(len(np.where((posits >= start_idx) & (posits < end_idx))[0]))
+                freqs.append(len(np.where((np.array(posits) >= start_idx) & (np.array(posits) < end_idx))[0]))
             word_list.append(w)
             mu_list.append(np.mean(freqs))
             sigma_list.append(np.std(freqs))
             freq_list.append(np.sum(freqs))
             posit_list.append(posits)
 
-
+        print("optimizing...")
         res = self._run_optim(mu_list, sigma_list)
         error = np.sqrt(np.mean(np.power(taylor_objective(res[0], mu_list, sigma_list), 2)))
 
@@ -52,4 +53,7 @@ class TaylorLowRegressor():
 
 
 def taylor_objective(param, x, y):
+    """
+    \sigam \approx c \mu^{\alpha}
+    """
     return np.log10(y) - np.log10(param[0] * x ** param[1])
